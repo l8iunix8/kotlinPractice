033debc36beff9b581e2abaad616ea839702fe6a
Kevin <l8iunix8@gmail.com>
2019-12-25 22:20:34 +0800

    Initial commit

---
+++ src/main/java/com/play/Person.java
@@ -0,0 +1,7 @@
+package com.play;
+
+public class Person {
+    public void hello(){
+        System.out.println("MY FRIEND");
+    }
+}
---
+++ src/main/kotlin/com/play/Hello.kt
@@ -0,0 +1,14 @@
+package com.play
+
+fun main() {
+//    println("Hello Kotlin");
+//    Human().hello();
+    var h = Human();
+    h.hello();
+    
+}
+class Human{
+    fun hello(){
+        println("Hello Kotlin My Friend")
+    }
+}
